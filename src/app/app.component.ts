b477e69b4e9963d84910dfb1ac13aff75d38c48c
DESKTOP-GNL3ABG\Frederick Sandalo <frederick.sandalo@zoramteq.com>
2017-10-27 15:27:17 +0800

    Finished the Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent 
+{
+  title = 'Tour of Heroes';
+  hero: Hero = {id: 1,
+                name: 'Windstorm'
+               }
+}
+
+export class Hero
+{
+  id: number;
+  name: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms'; //Step 1: <-- NgModel lives here
+import { AppComponent }  from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // Step 2: add the imported directive to the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
